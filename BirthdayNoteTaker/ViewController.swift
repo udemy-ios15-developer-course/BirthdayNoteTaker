0396c70b970862dc1496d5128104809b9889d857
Chris Hand <cj.hand@gmail.com>
2022-08-01 22:04:05 -0700

    Very simple storage sample

--- BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var birthdate: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let storedName = UserDefaults.standard.object(forKey: "name") as? String
+        else {
+            print("could not read key 'name'")
+            return
+        }
+        
+        guard let storedDate = UserDefaults.standard.object(forKey: "birthdate") as? String
+        else {
+            print("could not read key 'birthdate'")
+            return
+        }
+        
+         name.text = storedName
+         birthdate.text = storedDate
     }
 
-
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        UserDefaults.standard.set(name.text!, forKey: "name")
+        UserDefaults.standard.set(birthdate.text!, forKey: "birthdate")
+    }
+    
 }
 
